b1c64c18ad412b1a33b444b3d007b40a832a4356
RawadKadi <rawadkady@gmail.com>
2023-03-13 20:28:12 +0200

    centering launch app button

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,18 +1,23 @@
-import { Grid, GridItem, Box, Text, Button, Image, Heading } from "@chakra-ui/react";
+import { Grid, GridItem, Box, Text, Button, Image, Heading,Flex } from "@chakra-ui/react";
+import {useBreakpointValue } from "@chakra-ui/react";
 import { FaArrowRight } from "react-icons/fa";
 import React from "react";
 import Brick from "../../assets/images/brick.png"
 
 const Header =()=> {
+  const texts = useBreakpointValue({ base: "center", md: "left"});
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
   return (
     <Box display="flex" justifyContent="center" alignItems="center">
       <Grid templateColumns={["1fr", "1fr", "repeat(2, 1fr)"]} gap={6}>
         <GridItem justifyContent="center" alignItems="center">
-          <Box textAlign="left" display="flex" flexDirection="column" justifyContent="center" h="100%" ml={{ base: "0", md: "35" }}>
-            <Heading as="h1" size="2xl" >Bricklayer DAO</Heading>
-            <Text mt={4} >
+          <Box  display="flex" flexDirection="column" justifyContent="center" h="100%" >
+            <Heading as="h1" size="2xl" textAlign={texts} >Bricklayer DAO</Heading>
+            <Text mt={4} textAlign={texts}>
               Bricklayer DAO is a REIT that operates on the blockchain and is governed by its token/coin holders
             </Text>
+            <Flex justify={isMobile ? "center" : "flex-start"}  >
             <Button mt={4}
               bg="white"
               color="black"
@@ -23,12 +28,14 @@ const Header =()=> {
               maxWidth={["50%", "170px", "170px"]}
               padding={["8px 20px", "12px 40px", "12px 40px"]}
               marginTop={["50px", "100px", "100px"]}
+              
             >
               Launch App
               <Box display="inline-block" position="relative" top="2px" >
                 <FaArrowRight style={{ marginLeft: "8px" }} />
               </Box>
             </Button>
+            </Flex>
           </Box>
         </GridItem>
         <GridItem>
