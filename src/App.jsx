cae10506a61af6773a2e85a0ecaa1b14aef7bc47
RawadKadi <rawadkady@gmail.com>
2023-03-13 19:14:57 +0200

    sliding section display configuration with image edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-//Chakra imports 
+//Chakra imports
 import { ChakraProvider, Slider } from "@chakra-ui/react";
 import customTheme from "./theme";
-import { Grid, GridItem,Image } from "@chakra-ui/react";
+import { Grid, GridItem, Image, Box } from "@chakra-ui/react";
 
 //Files imports
 import Header from "../src/component/header/header.jsx";
-import SliderSection from './component/slider-section/slider';
+import SliderSection from "./component/slider-section/slider";
 
 //Image imports
 import Magnifier from "../src/assets/images/magnifier.png";
@@ -16,17 +16,14 @@ function App() {
   return (
     <ChakraProvider theme={customTheme}>
       <Header />
-      <Grid templateColumns="70% 30%" gap={4} >
-      <GridItem>
-        <SliderSection />
-      </GridItem>
-      <GridItem>
-      <Image src={Magnifier} alt="Placeholder" maxWidth={["100%", "300px", "300px"]} margin={["50px 0 0 0", "50px 0 0 0", "50px 0 0 100px"]} />
-
-      </GridItem>
-    </Grid>
-      </ChakraProvider>
-  )
+     
+        <GridItem>
+          <SliderSection />
+        </GridItem>
+        
+      
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
--- src/component/slider-section/slider.jsx
+++ src/component/slider-section/slider.jsx
@@ -3,15 +3,31 @@ import Slider from "react-slick";
 
 //Chakra Imports
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
-import { IconButton, Box, Text, Heading, Button } from "@chakra-ui/react";
+import {
+  IconButton,
+  Box,
+  Text,
+  Heading,
+  Button,
+  useBreakpointValue,
+  Grid,
+  GridItem,
+  Image,
+} from "@chakra-ui/react";
 
 //Css imports
 import "../slider-section/slick-theme.css";
 import "../slider-section/slick.css";
 
+//image imports
+import Magnifier from "../../assets/images/magnifier.png";
+import Earth from "../../assets/images/earth.png";
+import Phone from "../../assets/images/phone.png";
+
 function PrevArrow(props) {
   //Left navigational arrow of the slider
   const { className, onClick } = props;
+
   return (
     <IconButton
       className={className}
@@ -20,7 +36,7 @@ function PrevArrow(props) {
       onClick={onClick}
       position="absolute"
       top="50%"
-      left="0"
+      left={useBreakpointValue({ base: -10, md: 0 })}
       transform="translateY(-50%)"
       borderRadius="full"
       borderStyle="solid"
@@ -44,7 +60,7 @@ function NextArrow(props) {
       onClick={onClick}
       position="absolute"
       top="50%"
-      right="0"
+      right={useBreakpointValue({ base: -10, md: 0 })}
       transform="translateY(-50%)"
       borderRadius="full"
       borderWidth="1.5px"
@@ -136,109 +152,77 @@ const SliderSection = () => {
     ),
     beforeChange: handleSlideChange,
   };
+  const images = [Magnifier, Earth, Phone];
 
   return (
-    <Box borderWidth="1px" borderColor="gray" p="4" marginTop="100px">
-      <Slider ref={sliderRef} {...settings}>
-        <Box p="20" textAlign="left">
-          <Heading as="h6" size="lg">
-            What is REIT ?
-          </Heading>
-          <Text marginTop="20px">
-            A REIT, or Real Estate Investment Trust, is a type of investment
-            company that owns and operates income-producing real estate assets.
-            REITs typically own a portfolio of properties that generate rental
-            income and subject to capital appreciation. REITs distribute at
-            least 90% of its taxable income to shareholders in the form of
-            dividends
-          </Text>
-          {!showText[0] && (
-            <Button onClick={() => handleReadMoreClick(0)} mt="4">
-              Read more
-            </Button>
-          )}
-          {showText[0] && (
-            <>
-              <Text mt="4">
-                Vivamus ultricies augue a mauris imperdiet, quis convallis
-                sapien efficitur. Nam tristique ligula vel urna venenatis
-                malesuada. Nam commodo, arcu quis facilisis laoreet, neque magna
-                pulvinar sapien, id dignissim mauris quam in massa. Sed in
-                tellus commodo, congue velit eu, posuere metus. Fusce bibendum
-                commodo metus non varius. Nam nec leo eget nisl pharetra iaculis
-                vel eu augue. Aenean a lectus eu nisl finibus ultricies.
-                Vestibulum nec nisl vestibulum, egestas turpis sed, rutrum est.
-                Morbi sit amet
-              </Text>
-            </>
-          )}
+    <Grid templateColumns="70% 30%" gap={4}>
+      <GridItem>
+        <Box borderWidth="1px" borderColor="gray" p="4" marginTop="100px">
+          <Slider ref={sliderRef} {...settings}>
+            {[0, 1, 2].map((slideIndex) => (
+              <Box
+                key={slideIndex}
+                p={{ base: "4", md: "20" }}
+                textAlign="left"
+              >
+                <Heading as="h6" size={{ base: "md", md: "lg" }}>
+                  {slideIndex === 0 && "What is REIT?"}
+                  {slideIndex === 1 && "How BDAO uses a DAO structure?"}
+                  {slideIndex === 2 && "How BDAO uses blockchain tech?"}
+                </Heading>
+                <Text mt={{ base: "2", md: "4" }}>
+                  {slideIndex === 0 &&
+                    "A REIT, or Real Estate Investment Trust, is a type of investment company that owns and operates income-producing real estate assets. REITs typically own a portfolio of properties that generate rental income and subject to capital appreciation. REITs distribute at least 90% of its taxable income to shareholders in the form of dividends"}
+                  {slideIndex === 1 &&
+                    "Under the DAO governance framework, the members acquire a democratic right to influence the strategic direction of the REIT’s expansion. Members can introduce lucrative real estate opportunities, exercise influence over portfolio development, asset disposal, Active Participant (AP) selection, and diversification proposals via innovative voting systems"}
+                  {slideIndex === 2 &&
+                    "BDAO utilizes blockchain technology and crypto currencies to provide a platform applicable for capital raising via digital assets, dividend distribution, asset management, automated compliance, investor management, custodial systems, and transparency."}
+                </Text>
+                {!showText[slideIndex] && (
+                  <Button
+                    onClick={() => handleReadMoreClick(slideIndex)}
+                    mt={{ base: "2", md: "4" }}
+                  >
+                    Read more
+                  </Button>
+                )}
+                {showText[slideIndex] && (
+                  <>
+                    <Text mt={{ base: "2", md: "4" }}>
+                      Vivamus ultricies augue a mauris imperdiet, quis convallis
+                      sapien efficitur. Nam tristique ligula vel urna venenatis
+                      malesuada. Nam commodo, arcu quis facilisis laoreet, neque
+                      magna pulvinar sapien, id dignissim mauris quam in massa.
+                      Sed in tellus commodo, congue velit eu, posuere metus.
+                      Fusce bibendum commodo metus non varius. Nam nec leo eget
+                      nisl pharetra iaculis vel eu augue. Aenean a lectus eu
+                      nisl finibus ultricies. Vestibulum nec nisl vestibulum,
+                      egestas turpis sed, rutrum est. Morbi sit amet
+                    </Text>
+                  </>
+                )}
+              </Box>
+            ))}
+          </Slider>
         </Box>
-        <Box p="20" textAlign="left">
-          <Heading as="h6" size="lg">
-            How BDAO uses a DAO structure ?
-          </Heading>
-          <Text marginTop="20px">
-            Under the DAO governance framework, the members acquire a democratic
-            right to influence the strategic direction of the REIT’s expansion.
-            Members can introduce lucrative real estate opportunities, exercise
-            influence over portfolio development, asset disposal, Active
-            Participant (AP) selection, and diversification proposals via
-            innovative voting systems
-          </Text>
-          {!showText[0] && (
-            <Button onClick={() => handleReadMoreClick(0)} mt="4">
-              Read more
-            </Button>
-          )}
-          {showText[0] && (
-            <>
-              <Text mt="4">
-                Vivamus ultricies augue a mauris imperdiet, quis convallis
-                sapien efficitur. Nam tristique ligula vel urna venenatis
-                malesuada. Nam commodo, arcu quis facilisis laoreet, neque magna
-                pulvinar sapien, id dignissim mauris quam in massa. Sed in
-                tellus commodo, congue velit eu, posuere metus. Fusce bibendum
-                commodo metus non varius. Nam nec leo eget nisl pharetra iaculis
-                vel eu augue. Aenean a lectus eu nisl finibus ultricies.
-                Vestibulum nec nisl vestibulum, egestas turpis sed, rutrum est.
-                Morbi sit amet
-              </Text>
-            </>
-          )}
+      </GridItem>
+      <GridItem>
+        <Box
+          display={{ base: "flex", md: "block" }}
+          flexDirection={{ base: "row", md: "column" }}
+        >
+          <Image                                                  //displaying the image on the right of the slider
+            src={images[currentSlide]}
+            alt="Example image"
+            objectFit="cover"
+            order={1}
+            ml={{ base: "-50px", md: 0 }}
+            bg="white"
+            mb={{ base: "30px", md: 0 }}
+          />
         </Box>
-        <Box p="20" textAlign="left">
-          <Heading as="h6" size="lg">
-            How BDAO uses blockchain tech ?
-          </Heading>
-          <Text marginTop="20px">
-            BDAO utilizes blockchain technology and crypto currencies to provide
-            a platform applicable for capital raising via digital assets,
-            dividend distribution, asset management, automated compliance,
-            investor management, custodial systems, and transparency.
-          </Text>
-          {!showText[0] && (
-            <Button onClick={() => handleReadMoreClick(0)} mt="4">
-              Read more
-            </Button>
-          )}
-          {showText[0] && (
-            <>
-              <Text mt="4">
-                Vivamus ultricies augue a mauris imperdiet, quis convallis
-                sapien efficitur. Nam tristique ligula vel urna venenatis
-                malesuada. Nam commodo, arcu quis facilisis laoreet, neque magna
-                pulvinar sapien, id dignissim mauris quam in massa. Sed in
-                tellus commodo, congue velit eu, posuere metus. Fusce bibendum
-                commodo metus non varius. Nam nec leo eget nisl pharetra iaculis
-                vel eu augue. Aenean a lectus eu nisl finibus ultricies.
-                Vestibulum nec nisl vestibulum, egestas turpis sed, rutrum est.
-                Morbi sit amet
-              </Text>
-            </>
-          )}
-        </Box>
-      </Slider>
-    </Box>
+      </GridItem>
+    </Grid>
   );
 };
 
